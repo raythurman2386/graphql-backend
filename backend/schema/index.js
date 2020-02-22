2b43b9dd186764e6c320dad3f649552448bef4ba
raythurman2386 <raymondthurman5@gmail.com>
2020-02-22 02:10:14 -0500

    initial query schema

--- backend/schema/index.js
+++ backend/schema/index.js
@@ -1,21 +1,33 @@
 const graphql = require('graphql')
 const { Tech, Job } = require('../models/Model')
-const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema } = graphql
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = graphql
 
 const TechType = new GraphQLObjectType({
   name: 'Tech',
   fields: () => ({
-    id: { type: GraphQLInt },
-    name: { type: GraphQLString }
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    jobs: {
+      type: new GraphQLList(JobType),
+      resolve(parent, args) {
+        return Job.findTechJobs(parent.id)
+      }
+    }
   })
 })
 
 const JobType = new GraphQLObjectType({
   name: 'Job',
   fields: () => ({
-    id: { type: GraphQLInt },
+    id: { type: GraphQLID },
     machine: { type: GraphQLString },
-    complaint: { type: GraphQLString }
+    complaint: { type: GraphQLString },
+    tech: {
+      type: TechType,
+      resolve(parent, args) {
+        return Tech.findById(parent.tech_id)
+      }
+    }
   })
 })
 
@@ -24,17 +36,29 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     tech: {
       type: TechType,
-      args: { id: { type: GraphQLInt } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        // code to get data from db
         // args.id
-        Tech.findBy({ id: args.id })
+        return Tech.findById(args.id)
       }
     },
     techs: {
-      type: [TechType],
+      type: new GraphQLList(TechType),
       resolve(parent, args) {
-        Tech.find()
+        return Tech.find()
+      }
+    },
+    job: {
+      type: JobType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Job.findById(args.id)
+      }
+    },
+    jobs: {
+      type: new GraphQLList(JobType),
+      resolve(parent, args) {
+        return Job.find()
       }
     }
   }
