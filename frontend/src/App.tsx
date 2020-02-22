6931bdec5718ebbe2b58ac2c26154790604843fc
raythurman2386 <raymondthurman5@gmail.com>
2020-02-22 14:30:55 -0500

    added apollo client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { client } from './apollo'
+import Jobs from './components/Jobs'
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className='App'>
+        <h1>Hello</h1>
+        <Jobs />
+      </div>
+    </ApolloProvider>
   )
 }
 
