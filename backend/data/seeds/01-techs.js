8171591762ebddd1ec7ccacc580255caa842236d
raythurman2386 <raymondthurman5@gmail.com>
2020-02-22 00:49:39 -0500

    seed files created and run

--- backend/data/seeds/01-techs.js
+++ backend/data/seeds/01-techs.js
@@ -1,13 +1,10 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async function (knex) {
+  await knex('techs').insert([
+    { name: 'Test1' },
+    { name: 'Test2' },
+    { name: 'Test3' },
+    { name: 'Test4' },
+    { name: 'Test5' }
+  ])
 };
--- backend/data/seeds/02-jobs.js
+++ backend/data/seeds/02-jobs.js
@@ -1,13 +1,15 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async function (knex) {
+  await knex('jobs').insert([
+    { machine: 'test', complaint: 'something', tech_id: 1 },
+    { machine: 'test', complaint: 'something', tech_id: 1 },
+    { machine: 'test2', complaint: 'something', tech_id: 2 },
+    { machine: 'test2', complaint: 'something', tech_id: 2 },
+    { machine: 'test3', complaint: 'something', tech_id: 3 },
+    { machine: 'test3', complaint: 'something', tech_id: 3 },
+    { machine: 'test4', complaint: 'something', tech_id: 4 },
+    { machine: 'test4', complaint: 'something', tech_id: 4 },
+    { machine: 'test5', complaint: 'something', tech_id: 5 },
+    { machine: 'test5', complaint: 'something', tech_id: 5 }
+  ])
 };
