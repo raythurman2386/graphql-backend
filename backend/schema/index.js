61a99a1a721af256e70560171a3ea4842754f887
raythurman2386 <raymondthurman5@gmail.com>
2020-02-22 03:26:18 -0500

    refactored schema

--- backend/schema/index.js
+++ backend/schema/index.js
@@ -1,39 +1,14 @@
 const graphql = require('graphql')
-const { Tech, Job } = require('../models/Model')
-const { GraphQLObjectType, GraphQLID, GraphQLSchema, GraphQLList } = graphql
-const { TechType, JobType } = require('../types')
+const { GraphQLObjectType, GraphQLSchema, } = graphql
+const techSchema = require('./techSchema')
+const jobSchema = require('./jobSchema')
 const { Mutation } = require('../mutations')
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-    tech: {
-      type: TechType,
-      args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
-        // args.id
-        return Tech.findById(args.id)
-      }
-    },
-    techs: {
-      type: new GraphQLList(TechType),
-      resolve(parent, args) {
-        return Tech.find()
-      }
-    },
-    job: {
-      type: JobType,
-      args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
-        return Job.findById(args.id)
-      }
-    },
-    jobs: {
-      type: new GraphQLList(JobType),
-      resolve(parent, args) {
-        return Job.find()
-      }
-    }
+    ...techSchema,
+    ...jobSchema
   }
 })
 
