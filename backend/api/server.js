c58c43c69da4824f4c221a2d328f13703cd7a0bd
raythurman2386 <raymondthurman5@gmail.com>
2020-02-22 14:30:17 -0500

    added cors

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,12 +1,23 @@
 const express = require('express')
+const cors = require('cors')
 const graphqlHTTP = require('express-graphql')
 const schema = require('../schema')
 
 const server = express()
+server.use(
+  cors({
+    origin: '*',
+    methods: 'GET,PUT,POST,DELETE',
+    preflightContinue: false
+  })
+)
 
-server.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
-}))
+server.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+)
 
 module.exports = server
