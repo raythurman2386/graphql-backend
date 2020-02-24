f005c5ba61cf0b79159fb2e1420367e4fca12b04
raythurman2386 <raymondthurman5@gmail.com>
2020-02-24 18:57:40 -0500

    basic form

--- frontend/src/components/AddJob.tsx
+++ frontend/src/components/AddJob.tsx
@@ -4,7 +4,19 @@ import { useMutation } from '@apollo/react-hooks'
 
 const AddJob = () => {
   const [addJob, { data }] = useMutation(ADD_JOB)
-  return <div></div>
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault()
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type='text' placeholder='Machine' />
+      <input type='text' placeholder='Complaint' />
+      <input type='number' placeholder='Tech ID' />
+      <button type='submit'>Add Job</button>
+    </form>
+  )
 }
 
 export default AddJob
