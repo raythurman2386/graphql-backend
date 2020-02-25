c53efc1992bdb4e9994bc5f559623bfbde2fc4ec
raythurman2386 <raymondthurman5@gmail.com>
2020-02-24 19:23:49 -0500

    added form to the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { client } from './apollo'
 import Jobs from './components/Jobs'
+import AddJob from './components/AddJob'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className='App'>
         <h1>Hello</h1>
         <Jobs />
+        <AddJob />
       </div>
     </ApolloProvider>
   )
