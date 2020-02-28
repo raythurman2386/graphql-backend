a875198cfb7d49609f55b9c4bb850663c41f6f1c
raythurman2386 <raymondthurman5@gmail.com>
2020-02-27 20:52:55 -0500

    added user table

--- backend/data/migrations/20200221235754_jobs.js
+++ backend/data/migrations/20200221235754_jobs.js
@@ -1,13 +1,18 @@
-
-exports.up = async function (knex) {
+exports.up = async function(knex) {
   await knex.schema.createTable('jobs', tbl => {
     tbl.increments('id')
     tbl.string('machine').notNullable()
     tbl.string('complaint').notNullable()
-    tbl.int('tech_id').references('id').inTable('techs').unsigned().onUpdate('CASCADE').onDelete('CASCADE')
+    tbl
+      .integer('tech_id')
+      .references('id')
+      .inTable('techs')
+      .unsigned()
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
   })
-};
+}
 
-exports.down = async function (knex) {
+exports.down = async function(knex) {
   await knex.schema.dropTableIfExists('jobs')
-};
+}
