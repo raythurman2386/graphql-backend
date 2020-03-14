00acef5b459dbe1030fd00ee5402174826ce922a
Raymond Thurman <raymondthurman5@gmail.com>
2020-03-14 01:01:16 -0400

    Feat: techs refactored to TS

--- src/data/seeds/01-techs.js
+++
@@ -1,9 +0,0 @@
-exports.seed = async function(knex) {
-  await knex('techs').insert([
-    { name: 'Test1' },
-    { name: 'Test2' },
-    { name: 'Test3' },
-    { name: 'Test4' },
-    { name: 'Test5' }
-  ])
-}
