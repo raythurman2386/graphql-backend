daa3f8d49da1f5a3ed1a4bde88c4bcdeae9c46eb
Raymond Thurman <raymondthurman5@gmail.com>
2020-03-15 23:28:52 -0400

    Fix: Updated tech seed to have tech names

--- src/data/seeds/01-techs.ts
+++ src/data/seeds/01-techs.ts
@@ -2,10 +2,12 @@ import * as Knex from 'knex'
 
 export async function seed(knex: Knex): Promise<any> {
   await knex('techs').insert([
-    { name: 'Test1' },
-    { name: 'Test2' },
-    { name: 'Test3' },
-    { name: 'Test4' },
-    { name: 'Test5' }
+    { name: 'Herb' },
+    { name: 'Jason' },
+    { name: 'Josh' },
+    { name: 'Devon' },
+    { name: 'Noah' },
+    { name: 'Brady' },
+    { name: 'Eddy' }
   ])
 }
