aaeaa5eebeab36d3b50949045c2df332d88bdcbd
Node Code Formatter <formatter@1337z.ninja>
2020-03-17 23:35:34 +0000

    Formatted code

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,12 +1,12 @@
-import { ApolloServer } from 'apollo-server';
-import typeDefs from '../types';
-import resolvers from '../resolvers';
+import { ApolloServer } from 'apollo-server'
+import typeDefs from '../types'
+import resolvers from '../resolvers'
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   introspection: process.env.NODE_ENV !== 'production' || false,
   playground: process.env.NODE_ENV !== 'production' || false
-});
+})
 
-export default server;
+export default server
