3a7821528bfdbbffaabfff5cf5219b00b018beaf
Raymond Thurman <raymondthurman5@gmail.com>
2020-03-26 17:41:50 -0400

    Feat: added minor security to api

--- api/server.ts
+++ api/server.ts
@@ -2,12 +2,17 @@ import 'dotenv/config';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { createConnection } from 'typeorm';
+import { limiter } from './../utils/rateLimit';
+import { speedLimiter } from './../utils/slowDown';
 import { buildSchema } from 'type-graphql';
 import { UserResolver } from '../resolvers/UserResolver';
 import { TechResolver } from '../resolvers/TechResolver';
 import { JobResolver } from '../resolvers/JobResolver';
 
 const app = express();
+
+app.use(limiter);
+app.use(speedLimiter);
 
 (async () => {
   app.get('/', (_req, res) =>
