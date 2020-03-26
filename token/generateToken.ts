f6fa7cc4ce79895d0654f15080fc969f63e74b29
Raymond Thurman <raymondthurman5@gmail.com>
2020-03-25 20:22:36 -0400

    Fix: fixed token function names

--- token/generateToken.ts
+++ token/generateToken.ts
@@ -2,13 +2,13 @@ import 'dotenv/config';
 import { sign } from 'jsonwebtoken';
 import { User } from '../entity/User';
 
-export const createAccessToken = (user: User) => {
+export const generateToken = (user: User) => {
   return sign({ userId: user.id }, process.env.JWT_SECRET!, {
     expiresIn: '15m'
   });
 };
 
-export const createRefreshToken = (user: User) => {
+export const generateRefreshToken = (user: User) => {
   return sign({ userId: user.id }, process.env.JWT_REFRESH_SECRET!, {
     expiresIn: '7d'
   });
