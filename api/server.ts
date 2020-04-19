b1c133b0f9df158f740dd045cf062e280f5c6b9b
Raymond Thurman <raymondthurman5@gmail.com>
2020-04-19 17:11:21 -0400

    Troubleshooting cors

--- api/server.ts
+++ api/server.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import cors from 'cors';
 import typeDefs from '../types';
 import resolvers from '../resolvers';
 
 const app = express();
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  })
+);
 
 const server = new ApolloServer({
   typeDefs,
