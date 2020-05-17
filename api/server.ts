618d5806914890cc16e07abc1d54354adb13a3bb
Node Code Formatter <formatter@1337z.ninja>
2020-05-17 01:35:16 +0000

    Formatted code

--- api/server.ts
+++ api/server.ts
@@ -1,20 +1,20 @@
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import typeDefs from "../types";
-import resolvers from "../resolvers";
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import typeDefs from '../types';
+import resolvers from '../resolvers';
 
 const app = express();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  introspection: process.env.NODE_ENV !== "production",
-  playground: process.env.NODE_ENV !== "production",
+  introspection: process.env.NODE_ENV !== 'production',
+  playground: process.env.NODE_ENV !== 'production'
 });
 
 server.applyMiddleware({
-  cors: { origin: "*", credentials: true },
-  app,
+  cors: { origin: '*', credentials: true },
+  app
 });
 
 export default app;
