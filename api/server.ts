96b34645df86f9ff6e2aae7465833618fee0c7cf
Raymond Thurman <raymondthurman5@gmail.com>
2020-05-16 21:32:25 -0400

    fix: completely removed cors

--- api/server.ts
+++ api/server.ts
@@ -1,24 +1,19 @@
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import typeDefs from '../types';
-import resolvers from '../resolvers';
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import typeDefs from "../types";
+import resolvers from "../resolvers";
 
 const app = express();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  introspection: process.env.NODE_ENV !== 'production',
-  playground: process.env.NODE_ENV !== 'production',
+  introspection: process.env.NODE_ENV !== "production",
+  playground: process.env.NODE_ENV !== "production",
 });
 
 server.applyMiddleware({
-  cors: {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: true,
-    optionsSuccessStatus: 204,
-  },
+  cors: false,
   app,
 });
 
